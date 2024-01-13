8b0a6a1b4d0874ec6cd2f2a80a7d4ec4a5e7c42b
Santiago Fisela <santyfisela@gmail.com>
2024-01-13 17:46:29 -0300

    Renamed Anims and made more
    There's still a lot of animations to make wooooooo

---
+++ Assets/Scripts/BodyPartsManager.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartsManager : MonoBehaviour
+{
+    [SerializeField] private CharacterBodySO characterBody;
+
+    [Header("String arrays")]
+    [SerializeField] private string[] bodyPartTypes;
+    [SerializeField] private string[] characterStates;
+    [SerializeField] private string[] characterDirections;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+    private AnimationClip animationClip;
+    private AnimatorOverrideController animatorOverrideController; 
+    private AnimationClipOverrides defaultAnimationClips;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = animatorOverrideController;
+
+        defaultAnimationClips = new AnimationClipOverrides(animatorOverrideController.overridesCount);
+        animatorOverrideController.GetOverrides(defaultAnimationClips);
+    }
+
+    public void UpdateBodyParts()
+    {
+        for (int partIndex = 0; partIndex < bodyPartTypes.Length; partIndex++)
+        {
+            string partType = bodyPartTypes[partIndex];
+
+            string partID = characterBody.characterBodyParts[partIndex].bodyPart.bodyPartAnimationID.ToString();
+
+            for (int stateIndex = 0; stateIndex < characterStates.Length; stateIndex++)
+            {
+                string state = characterStates[stateIndex];
+                for (int directionIndex = 0; directionIndex < characterDirections.Length; directionIndex++)
+                {
+                    string direction = characterDirections[directionIndex];
+                    // The naming convention is: "[Type]_[Index]_[state]_[direction]" (Ex. Body_0_Idle_Down)
+                    animationClip = Resources.Load<AnimationClip>("Animations/" + "Player/" + partType + "/" + partType + "_" + partID + "_" + state + "_" + direction);
+
+                    defaultAnimationClips[partType + "_" + 0 + "_" + state + "_" + direction] = animationClip;
+                }
+            }
+        }
+
+        animatorOverrideController.ApplyOverrides(defaultAnimationClips);
+    }
+
+    public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>
+    {
+        public AnimationClipOverrides(int capacity) : base(capacity) { }
+
+        public AnimationClip this[string name]
+        {
+            get { return this.Find(x => x.Key.name.Equals(name)).Value; }
+            set
+            {
+                int index = this.FindIndex(x => x.Key.name.Equals(name));
+                if (index != -1)
+                    this[index] = new KeyValuePair<AnimationClip, AnimationClip>(this[index].Key, value);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/BodyPartsSelector.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BodyPartsSelector : MonoBehaviour
+{
+    [SerializeField] private CharacterBodySO characterBody;
+    [SerializeField] private BodyPartSelection[] bodyPartSelections;
+
+    private void Start()
+    {
+        for (int i = 0; i < bodyPartSelections.Length; i++)
+        {
+            GetCurrentBodyParts(i);
+        }
+    }
+
+    public void NextBodyPart(int partIndex)
+    {
+        if (ValidateIndexValue(partIndex))
+        {
+            if (bodyPartSelections[partIndex].bodyPartCurrentIndex < bodyPartSelections[partIndex].bodyPartOptions.Length - 1)
+            {
+                bodyPartSelections[partIndex].bodyPartCurrentIndex++;
+            }
+            else
+            {
+                bodyPartSelections[partIndex].bodyPartCurrentIndex = 0;
+            }
+
+            UpdateCurrentPart(partIndex);
+        }
+    }
+
+    public void PreviousBody(int partIndex)
+    {
+        if (ValidateIndexValue(partIndex))
+        {
+            if (bodyPartSelections[partIndex].bodyPartCurrentIndex > 0)
+            {
+                bodyPartSelections[partIndex].bodyPartCurrentIndex--;
+            }
+            else
+            {
+                bodyPartSelections[partIndex].bodyPartCurrentIndex = bodyPartSelections[partIndex].bodyPartOptions.Length - 1;
+            }
+
+            UpdateCurrentPart(partIndex);
+        }
+    }
+
+    private bool ValidateIndexValue(int partIndex)
+    {
+        if (partIndex > bodyPartSelections.Length || partIndex < 0)
+        {
+            Debug.LogError("Invalid part index (Does not mach a body part)");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void GetCurrentBodyParts(int partIndex)
+    {
+        bodyPartSelections[partIndex].bodyPartNameTextComponent.text = characterBody.characterBodyParts[partIndex].bodyPart.bodyPartName;
+        bodyPartSelections[partIndex].bodyPartCurrentIndex = characterBody.characterBodyParts[partIndex].bodyPart.bodyPartAnimationID;
+    }
+
+    private void UpdateCurrentPart(int partIndex)
+    {
+        bodyPartSelections[partIndex].bodyPartNameTextComponent.text = bodyPartSelections[partIndex].bodyPartOptions[bodyPartSelections[partIndex].bodyPartCurrentIndex].bodyPartName;
+        characterBody.characterBodyParts[partIndex].bodyPart = bodyPartSelections[partIndex].bodyPartOptions[bodyPartSelections[partIndex].bodyPartCurrentIndex];
+    }
+}
+
+[System.Serializable]
+public class BodyPartSelection
+{
+    public string bodyPartName;
+    public BodyPartSO[] bodyPartOptions;
+    public Text bodyPartNameTextComponent;
+    [HideInInspector] public int bodyPartCurrentIndex;
+}
